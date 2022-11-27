53fd514469959e6cffac785e878e1bce50712875
Anna <anna.pak.vl@gmail.com>
2022-11-27 22:17:58 +0300

    added: footer (not done yet)

---
+++ js/review.js 13-35-15-328.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper-review', {
+  // Optional parameters
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
